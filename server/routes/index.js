b549ecf9d36a99f73a52967fad6e380a1766b44f
Hzyhhh <799309084@qq.com>
2019-04-25 17:04:40 +0800

    add new API

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getUuid', function (req, res) {
+  console.log("调用/getUuid获取beacon_devices设备信息")
   let sql = `select * from beacon_devices;`;
 
   conn.query(sql, (err, data)=>{
@@ -22,7 +23,25 @@ router.get('/getUuid', function (req, res) {
   })
 });
 
+router.post('/postLocated', function(req, res){
+  console.log(req.body)
+  let sql = `insert into `
+})
+
+router.get('/WeChat/getLocation', (req, res)=>{
+  console.log("调用/WeChat/getLocation接口获取beacon_upload位置信息")
+  let sql = `select * from beacon_upload`
+
+  conn.query(sql, (err, data)=>{
+    if(err)
+      throw err;
+    else
+      res.send(data)
+  })
+})
+
 router.get('/getUser', function(req, res) {
+  console.log("调用/getUser接口获取beacon_user用户信息")
   let sql = `select * from beacon_user;`;
 
   conn.query(sql, (err, data)=>{
